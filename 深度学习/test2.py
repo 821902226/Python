e1ef98e3cd81f829aa87b6ed2c06376ad2739b6f
simple <821902226@qq.com>
2022-07-17 21:34:45 -0700

    tensorflow2.0高阶API(tf.keras)

--- 深度学习/test2.py
+++ 深度学习/test2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # 1.构建神经网络模型
 model = tf.keras.Sequential()
-# 添加一层全连接神经网络
+# 添加一层全连接神经网络(Dense表示全连接层)
 model.add(tf.keras.layers.Dense(input_dim=1, units=1))
 # 对神经网络模型进行编译
 model.compile(loss='mse', optimizer='sgd')
@@ -12,7 +12,7 @@ model.compile(loss='mse', optimizer='sgd')
 # 2.训练神经网络模型
 x = np.linspace(-10, 10, 500)
 y = 2 * x + 100 + tf.random.normal([500, 1], 0, 1)
-# 开始训练
+# 开始训练(epochs是迭代次数， validation_split是测试集的比例)
 model.fit(x, y, verbose=1, epochs=200, validation_split=0.2)
 
 # 3.保存神经网络模型
